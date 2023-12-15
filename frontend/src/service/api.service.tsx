ce994ba23e967c35f466921e08c51902340b2587
Lilian I Meza Gaidtov <80003324+Liliani-mg@users.noreply.github.com>
2023-12-15 15:24:25 -0300

    Update api.service.tsx

--- frontend/src/service/api.service.tsx
+++ frontend/src/service/api.service.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const BASE_URL = "https://poke-api-tyqw.onrender.com";
 
 export const fetchPokemons = async (page = 1, limit = 15) => {
   try {
